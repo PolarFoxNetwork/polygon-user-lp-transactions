1b5f13cba9694f6d6c5ef552d252393cb208ae62
apyvision <team@apy.vision>
2021-05-20 17:29:09 -0700

    fixeed a bug with the graph

--- src/elk/factory.ts
+++ src/elk/factory.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prefer-const */
 import {PairCreated} from '../../generated/SushiswapFactory/Factory'
 import {log} from "@graphprotocol/graph-ts/index";
-import {ComethPair as PairTemplate} from '../../generated/templates'
+import {ElkPair as PairTemplate} from '../../generated/templates'
 
 export function handleNewPair(event: PairCreated): void {
   // create the tracked contract based on the template
